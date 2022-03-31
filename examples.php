8936192837e53fb575217a85dae476c7fecea4d6
Artem Kuznecov <artem.kuznecov.samara@yandex.ru>
2022-03-31 13:11:03 +0400

    upd

---
+++ examples.php
@@ -0,0 +1,18 @@
+<?
+
+get_header();
+  get_header('test');// = header-test.php
+
+get_footer();
+  get_footer('test');// = footer-test.php
+
+get_sidebar();
+
+
+
+get_template_part('some-dir/some-template');
+get_post_format(); //'aside', 'chat', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio'
+
+
+
+is_home();
