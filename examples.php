ed8694b5b4a43b0a9f4bec69a4e2acb9692f561f
Artem Kuznecov <artem.kuznecov.samara@yandex.ru>
2022-04-03 21:34:06 +0400

    upd

--- examples.php
+++ examples.php
@@ -1,6 +1,37 @@
 <?
 
-<form action="<?=admin_url('admin-ajax.php?action=our_send_mail_handler');?>"></form>/* our_send_mail_handler in functions.php*/
+<form action="<?=admin_url('admin-ajax.php?action=custom_send_mail');?>"></form>/* custom_send_mail in functions.php*/
+
+add_action('wp_ajax_custom_send_mail', 'custom_send_mail');
+add_action('wp_ajax_nopriv_custom_send_mail', 'custom_send_mail');
+
+function custom_send_mail()
+{
+  // подразумевается что $to, $subject, $message уже определены...
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+  $to = get_option('admin_email');
+
+  $message = $message . ' ' . $name . ' ' . $email;
+
+  // удалим фильтры, которые могут изменять заголовок $headers
+  remove_all_filters( 'wp_mail_from' );
+  remove_all_filters( 'wp_mail_from_name' );
+
+  $headers = array(
+    'From: noreply <noreplay@'.$_SERVER['HTTP_HOST'].'>',
+    'content-type: text/html',
+    // 'cc: John 1 Codex <jqc@wordpress.org>',
+    // 'cc: John 2 Codex <j2qc@wordpress.org>',
+    // 'bcc: iluvwp@wordpress.org', // тут можно использовать только простой email адрес
+  );
+
+  wp_mail($to, $subject, $message, $headers);
+}
+
+
 
 get_header();
 get_header('test'); // = header-test.php
